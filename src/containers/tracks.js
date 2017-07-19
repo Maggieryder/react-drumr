bfd6e633c55b7b5935cbdd34576d57525ead7ff8
maggieryder <maggieryder@mac.com>
2017-07-19 09:09:15 -0400

    Minor clean up

--- src/containers/tracks.js
+++ src/containers/tracks.js
@@ -2,8 +2,8 @@ import Tracks from '../components/tracks'
 import { connect } from 'react-redux'
 import { addTrack, removeTrack } from '../actions'
 
-function mapStateToProps({ tracks, kits }){
-  return { tracks, kits }
+function mapStateToProps({ tracks }){
+  return { tracks }
 }
 
 export default connect(mapStateToProps, { addTrack, removeTrack })(Tracks)
