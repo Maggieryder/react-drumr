074ba535d6f17aa50c538f3e7039a96990129f98
maggieryder <maggieryder@mac.com>
2017-07-29 16:33:20 -0400

    Clean up B

--- src/components/tracks.jsx
+++ src/components/tracks.jsx
@@ -6,17 +6,13 @@ let nextTrackId = 0;
 
 const Tracks = ({ tracks, addTrack, drumr }) => {
 
-  const addNewTrack = (id) => {
-    // drumr.addTrack(id)
-    addTrack({id:id})
-  }
   const soloActive = () => {
     return tracks.filter(t => t.solo).length > 0
   }
   return (
     <ul className="tracks">
       <li className="track">
-        <a className='addTrackBtn' href='#' onClick={ () => addNewTrack(nextTrackId++) }>+</a> ADD TRACK
+        <a className='addTrackBtn' href='#' onClick={ () => addTrack({id:nextTrackId++}) }>+</a> ADD TRACK
       </li>
       {tracks.map((track, i) => <Track key={i} track={track} drumr={drumr} soloActive={soloActive()}/>)}
     </ul>
